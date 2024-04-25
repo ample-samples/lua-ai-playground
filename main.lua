1ce1652d1514b85ebd6825be0615ee676e473653
Todd <todd.griffin61@outlook.com>
2024-04-25 08:54:12 +0100

    trying things

--- main.lua
+++ main.lua
@@ -15,20 +15,30 @@ local bias1 = 2
 local bias2 = 3
 local bias3 = 0.5
 
-local biases = matrix { bias1, bias2, bias3 }
+local biases = { bias1, bias2, bias3 }
 
+-- calculateLayoutOutput takes 3 params:
+--     layerInputs = the outputs from the nodes of the previous layer
+--     layerWeights = a table of weights, each table corresponding to a node in the working layer and each value in that table corresponding to the weight associated with a node from the previous layer
+--     layerBiases = the number to add to the dot of 
 local function calculateLayoutOutput(layerInputs, layerWeights, layerBiases)
 	local layerOutput = {};
 	for i = 1, #layerBiases do
-		nodeSumWeightXInput = 0
 
+		-- basically the dot product of layerWeights and layerInputs
+		local nodeSumWeightXInput = 0
 		for j = 1, #layerInputs, 1 do
 			nodeSumWeightXInput = nodeSumWeightXInput + layerWeights[i][j] * layerInputs[j]
 		end
-		layerOutput[i] = layerBiases[i][1] + nodeSumWeightXInput
+		nodeSumWeightXInput = matrix.dot(layerWeights, layerInputs)
+		layerOutput[i] = layerBiases[i] + nodeSumWeightXInput
 	end
 
 	return matrix(layerOutput)
 end
 
 print(calculateLayoutOutput(inputs, weights, biases))
+
+
+
+
